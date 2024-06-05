4b19e1562b11c693d0883f3f17b50c49e4dac724
MathildeMarieDuville <mathilde.duville@gmail.com>
2024-06-05 20:48:13 +0200

    Graphical User Interface (GUI) for Multiple Object Tracking in PsychoPy

---
+++ PsychoPy/MultipleObjectTracking/locales/template/create_pot.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\PsychoPy3\python.exe" ../python3/Tools/i18n/pygettext.py ../../MOTTest.py
+
+pause
---
+++ PsychoPy/MultipleObjectTracking/locales/translated/update_po_to_mo.bat
@@ -0,0 +1,3 @@
+for /r %%f in (*) do "C:\Program Files\PsychoPy3\python.exe" "../python3/Tools/i18n/msgfmt.py" %%f
+
+pause
